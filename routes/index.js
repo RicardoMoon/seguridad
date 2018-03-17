8eac95e887734b0e13497abff9615009d931c7c6
RicardoMoon <thamokerking@hotmail.com>
2018-03-17 02:17:23 -0400

    second push

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Diseño y Seguridad' });
+  res.render('index');
 });
 
 module.exports = router;
