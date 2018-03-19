eb58be8273c13880d39bacef445e01a63a8f4325
RicardoMoon <thamokerking@hotmail.com>
2018-03-18 21:50:21 -0400

    fourth push

---
+++ public/assets/js/scripts.js
@@ -0,0 +1,23 @@
+jQuery(function ($) {
+
+    'use strict';
+
+    // -------------------------------------------------------------
+    // Countdown
+    // -------------------------------------------------------------
+    (function () {
+
+        $("#back-countdiown").countdown({
+            date: "23 march 2018 7:00:00",
+            format: "on"
+        });
+    
+    }()); 
+
+
+}); // JQuery end
+
+
+$(document).on('click', '.m-menu .dropdown-menu', function(e) {
+  e.stopPropagation()
+})
